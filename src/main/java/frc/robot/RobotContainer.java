d89079e48aaac43c8d0b076c41aef0289260e97c
MattD8957 <Dowlmj8957@gmail.com>
2023-12-01 13:17:07 -0500

    Added second motor to test gearbox

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,9 +13,9 @@ import frc.thunder.LightningContainer;
 import frc.thunder.auto.AutonomousCommandFactory;
 
 public class RobotContainer extends LightningContainer {
-    private static final Swerve drivetrain = new Swerve();
+    // private static final Swerve drivetrain = new Swerve();
 //     private static final LimelightSubsystem limelight = new LimelightSubsystem();
-        // private static final TestMotor testMotor = new TestMotor();
+        private static final TestMotor testMotor = new TestMotor();
         
     private static final XboxController driver =
             new XboxController(ControllerConstants.DRIVER_CONTROLLER_PORT);
@@ -35,10 +35,10 @@ public class RobotContainer extends LightningContainer {
 
     @Override
     protected void configureDefaultCommands() {
-        drivetrain.setDefaultCommand(
-                new TeleopSwerve(drivetrain, () -> driver.getLeftY(), () -> driver.getLeftX(),
-                        () -> driver.getRightX(), () -> (driver.getRightTriggerAxis() > 0.75)));
-        // testMotor.setDefaultCommand(new motorTest(() -> driver.getRightY(), testMotor));
+        // drivetrain.setDefaultCommand(
+        //         new TeleopSwerve(drivetrain, () -> driver.getLeftY(), () -> driver.getLeftX(),
+        //                 () -> driver.getRightX(), () -> (driver.getRightTriggerAxis() > 0.75)));
+        testMotor.setDefaultCommand(new motorTest(() -> driver.getRightY(), testMotor));
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/TestMotor.java
+++ src/main/java/frc/robot/subsystems/TestMotor.java
@@ -4,18 +4,25 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.REVConfigs;
+import frc.thunder.config.FalconConfig;
 import frc.thunder.shuffleboard.LightningShuffleboard;
 
 public class TestMotor extends SubsystemBase {
-  private CANSparkMax motor = new CANSparkMax(6, MotorType.kBrushless);
+  // private CANSparkMax motor = new CANSparkMax(6, MotorType.kBrushless);
+  private TalonFX motor1;
+  private TalonFX motor2;
 
   public TestMotor() {
     // motor = REVConfigs.configDriveMotor(motor);
-    motor = REVConfigs.configAngleMotor(motor);
+    // motor = REVConfigs.configAngleMotor(motor);
+    motor1 = FalconConfig.createMotor(10, false, 0, 0, NeutralModeValue.Coast);
+    motor2 = FalconConfig.createMotor(11, false, 0, 0, NeutralModeValue.Coast);
   }
 
   @Override
@@ -24,7 +31,8 @@ public class TestMotor extends SubsystemBase {
   }
 
   public void setPower(double pow){
-    motor.set(pow);
+    motor1.set(pow);
+    motor2.set(pow);
   }
 
   public void stop(){
